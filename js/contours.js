3d909eb3b23512d7f4a8f37428133bfc2e4391fa
LaVielle <Sam@Samuels-MacBook-Pro.local>
2017-02-07 18:05:09 -0500

    minor changes

--- js/contours.js
+++ js/contours.js
@@ -1,10 +1,12 @@
-
 function drawContours(){
   console.log("contours.js -running...")
 
   var data = blobMap;
 
   // Add a "cliff edge" to force contour lines to close along the border.
+  
+  /*
+
   var cliff = -1000;
   data.push(d3.range(data[0].length).map(function() { return cliff; }));
   data.unshift(d3.range(data[0].length).map(function() { return cliff; }));
@@ -12,6 +14,8 @@ function drawContours(){
     d.push(cliff);
     d.unshift(cliff);
   });
+  
+  */
 
   var c = new Conrec,
       xs = d3.range(0, data.length),
@@ -19,9 +23,9 @@ function drawContours(){
       zs = d3.range(0, 1, 1),
       width = canvas.width,
       height = canvas.height,
-      x = d3.scaleLinear().range([width, 0]).domain([data.length, 0]),
-      y = d3.scaleLinear().range([height, 0]).domain([data[0].length, 0]),
-      colours = d3.scaleLinear().domain([-5, 3]).range(["#fff", "red"]);
+      x = d3.scale.linear().range([0, width]).domain([data.length, 0]),
+      y = d3.scale.linear().range([height, 0]).domain([data[0].length, 0]),
+      colours = d3.scale.linear().domain([-5, 3]).range(["#fff", "red"]);
 
   c.contour(data, 0, xs.length - 1, 0, ys.length - 1, xs, ys, zs.length, zs);
 
@@ -32,13 +36,10 @@ function drawContours(){
       .data(c.contourList())
     .enter().append("path")
       //.style("fill",function(d) { return colours(d.level);})
-      //.style("fill", "white")
-      .style("stroke","red")
-
-      .style("stroke-width", 2)
       .style("fill", "none")
+      .style("stroke","black")
 
-      .attr("d", d3.line()
+      .attr("d", d3.svg.line()
         .x(function(d) { return x(d.x); })
         .y(function(d) { return y(d.y); }));
 
